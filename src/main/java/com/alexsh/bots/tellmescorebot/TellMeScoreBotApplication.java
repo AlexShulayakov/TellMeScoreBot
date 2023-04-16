ae5efba25326c6bf0d9b2a54dd69cc2a46ea215f
Alex Shulayakov <shaakem@shaakem.com>
2023-04-16 21:10:51 +0700

    Initial commit

---
+++ src/main/java/com/alexsh/bots/tellmescorebot/TellMeScoreBotApplication.java
@@ -0,0 +1,12 @@
+package com.alexsh.bots.tellmescorebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TellMeScoreBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TellMeScoreBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexsh/bots/tellmescorebot/TellMeScoreBotApplicationTests.java
@@ -0,0 +1,12 @@
+package com.alexsh.bots.tellmescorebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TellMeScoreBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
